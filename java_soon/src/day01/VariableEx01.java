a0b84d9ee6a04b13ca5f5b86b4b36f4e8cb04ed2
soon <sooooon3416@gmail.com>
2022-10-27 18:54:38 +0900

    day01

---
+++ java_soon/src/day01/VariableEx01.java
@@ -0,0 +1,27 @@
+package day01;
+
+public class VariableEx01 {
+
+	public static void main(String[] args) {
+		/* - 변수선언
+		 * 자료형 변수명;
+		 * 자료형 변수명 = 초기값;
+		 * - 자료형
+		 */
+		
+		boolean isEven = false;
+		char operator = '+';// 캐릭터에 저장하려면 무조건 한자여야함, 쌍따옴표 사용 불가
+	    int age = 30;
+		float pi1 = 3.1315f; //float은 접미사F나 를 반드시 붙여야 한다
+		double pi2 = 3.1415;
+		//참조형 문자열
+		String str = "+"; //꼭 쌍따옴표
+		
+		//사과의 개수를 저장하기 위한 변수를 선언해보세요
+		
+		
+		//학생 평균 성적을 저장하기 위한 변수를 선언해보세요.
+
+	}
+
+}
