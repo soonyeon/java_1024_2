3784203f1aad7af779df6382de944a6c0a156e7a
soon <sooooon3416@gmail.com>
2022-12-27 21:40:03 +0900

    db_대학생 관리 프로그램

---
+++ java_soon/src/db/day3/UniversityProgramMain.java
@@ -0,0 +1,12 @@
+package db.day3;
+
+public class UniversityProgramMain {
+
+	public static void main(String[] args) {
+		UniversityController uc = new UniversityController();
+		
+				
+
+	}
+
+}
---
+++ java_soon/src/db/day3/dao/StudentDAO.java
@@ -0,0 +1,14 @@
+package db.day3.dao;
+
+import db.day3.vo.StudentVO;
+
+public interface StudentDAO {
+	
+	public void insertStudent(StudentVO std);
+	
+	public void updateStudent(StudentVO std);
+
+		
+	
+
+}
