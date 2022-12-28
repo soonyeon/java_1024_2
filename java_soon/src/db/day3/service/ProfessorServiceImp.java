62474ff12cabbdf96c668b8e85a00f172b8c7160
soon <sooooon3416@gmail.com>
2022-12-28 21:25:09 +0900

    db_04

--- java_soon/src/db/day3/service/ProfessorServiceImp.java
+++ java_soon/src/db/day3/service/ProfessorServiceImp.java
@@ -3,20 +3,21 @@ package db.day3.service;
 import java.util.Scanner;
 
 import db.day3.DBConnector;
-import db.day3.dao.ProfessorMapper;
-import db.day3.dao.StudentMapper;
-import db.day3.vo.ProfessorVO;
-import db.day3.vo.StudentVO;
+import db.day3.dao.*;
+import db.day3.vo.*;
 
-public class ProfessorServiceImp implements ProfessorService{
-	
+
+public class ProfessorServiceImp implements ProfessorService {
+
 	private ProfessorMapper professorDao;
+	private StudentMapper studentDao;
 	private Scanner scan = new Scanner(System.in);
 	
 	public ProfessorServiceImp(DBConnector dbConnector) {
-		professorDao = new  ProfessorMapper(dbConnector);
+		professorDao = new ProfessorMapper(dbConnector);
+		studentDao = new StudentMapper(dbConnector);
 	}
-	
+
 	public void insertProfessor() {
 		System.out.println("추가할 교수 정보 입력하세요.");
 		System.out.print("교번 : ");
@@ -50,9 +51,19 @@ public class ProfessorServiceImp implements ProfessorService{
 				pr_state, pr_de_num, pr_tell); 
 		professorDao.updateProfessor(professorVO);
 		
+	}
 
-
-	
-		
+	public void updateAdvisor() {
+		System.out.print("교수번호 : ");
+		String st_pr_num = scan.nextLine();
+		System.out.print("학번 : ");
+		String st_num = scan.nextLine();
+		StudentVO std = studentDao.selectStudent(st_num);
+		if(std == null) {
+			System.out.println("없는 학번이어서 지도 교수 수정에 실패했습니다.");
+			return;
+		}
+		std.setSt_pr_num(st_pr_num);
+		studentDao.updateStudent(std);
 	}
 }
