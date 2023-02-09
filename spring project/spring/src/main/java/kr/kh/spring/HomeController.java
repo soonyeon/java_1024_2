d64a6897ded51af9c6443132009464147bc3bd7a
soon <sooooon3416@gmail.com>
2023-02-09 18:21:56 +0900

    스프링_02

--- spring project/spring/src/main/java/kr/kh/spring/HomeController.java
+++ spring project/spring/src/main/java/kr/kh/spring/HomeController.java
@@ -1,57 +1,47 @@
 package kr.kh.spring;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Handles requests for the application home page.
- */
+
 @Controller
 public class HomeController {
 	
 	@RequestMapping(value = "/")
-	public ModelAndView home(ModelAndView mv,String name, Integer age) {
-		System.out.println(name);
-		mv.addObject("name1",name);
-		mv.addObject("age",age);
+	public ModelAndView home(ModelAndView mv) {
 		mv.setViewName("home");
 		return mv;
 	}
 	
-	@RequestMapping(value = "/board/{num}")
-	public ModelAndView board(ModelAndView mv,@PathVariable("num")Integer num1) {
-		System.out.println("게시글 번호 : " + num1); 
-		mv.setViewName("home");
+	@RequestMapping(value = "/ex1")
+	public ModelAndView ex1(ModelAndView mv,String name, Integer age) {
+		System.out.println("예제1 - 화면에서 전달한 이름 : " + name);
+		System.out.println("예제1 - 화면에서 전달한 나이 : " + age);
+		mv.setViewName("ex1");
 		return mv;
 	}
-	
-	@RequestMapping(value = "/test")
-	public ModelAndView home(ModelAndView mv,InfoVo info) {
-		mv.addObject("info1",info);
-		mv.setViewName("home2");
+	@RequestMapping(value = "/ex2")
+	public ModelAndView ex2(ModelAndView mv,String name, Integer age) {
+		System.out.println("예제2 - 화면에서 전달한 이름 : " + name);
+		System.out.println("예제2 - 화면에서 전달한 나이 : " + age);
+		mv.setViewName("ex2");
 		return mv;
 	}
-	
-	@RequestMapping(value = "/login",method=RequestMethod.GET)
-	public ModelAndView login(ModelAndView mv) {
-		mv.setViewName("login"); //화면 이름
+	@RequestMapping(value = "/ex3")
+	public ModelAndView ex3(ModelAndView mv) {
+		mv.setViewName("ex3");
 		return mv;
 	}
-	@RequestMapping(value = "/login",method=RequestMethod.POST)
-	public ModelAndView login(ModelAndView mv,String id, String pw) {
-		System.out.println("id");
-		System.out.println("pw");
-		mv.setViewName("login"); //화면 이름
+	@RequestMapping(value = "/ex3/{name}/{age}")
+	public ModelAndView exNameAge3(ModelAndView mv,
+			@PathVariable("name")String name,
+			@PathVariable("age")int age) {
+		System.out.println("예제3 - 화면에서 전달한 이름 : " + name);
+		System.out.println("예제3 - 화면에서 전달한 나이 : " + age);
+		mv.setViewName("ex3");
 		return mv;
 	}
 }
